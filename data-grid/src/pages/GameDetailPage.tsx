b1a3395f5714cf1617a31b07c3c1daec89eb06b9
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-03-04 10:28:47 +1100

    Added detail page

--- data-grid/src/pages/GameDetailPage.tsx
+++ data-grid/src/pages/GameDetailPage.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { Heading, Image, Text } from "@chakra-ui/react";
+import useGame from "../hooks/useGame";
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
-    return (
-        <>
-            
-        </>
-    );
-}
+  const { slug } = useParams<{ slug: string }>();
+  const { data: game, error, isLoading } = useGame(slug!);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error || !game) {
+    return <div>Error: {error.message}</div>;
+  }
+  return (
+    //  I need to get a game slug and title and description and image and rating
+    // write a query to get the game details
+
+    <>
+      <Heading>{game?.name}</Heading>
+      <Text>{game?.description_raw}</Text>
+      {/* <Image src={getCroppedImageUrl(game.background_image)} alt={game?.name} /> */}
+    </>
+  );
+};
 
 export default GameDetailPage;
