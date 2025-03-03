a51c5e9d721c93ae7def775b2b4c175cadc02947
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-03-03 15:08:34 +1100

    Added routes

--- data-grid/src/main.tsx
+++ data-grid/src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
 import theme from "./theme";
 import "./index.css";
 
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import router from "./services/routes";
+import { RouterProvider } from "react-router-dom";
 Amplify.configure(outputs);
 
 const queryClient = new QueryClient();
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router}></RouterProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
