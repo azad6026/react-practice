0e30c4fe2c84078073fb6c247770cabe55122864
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-03-04 18:56:51 +1100

    Added trailer component

---
+++ data-grid/src/components/GameTrailer.tsx
@@ -0,0 +1,31 @@
+import { Box } from "@chakra-ui/react";
+import useTrailer from "../hooks/useTrailer";
+import React from "react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailer(gameId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error || !data) {
+    return <div>Error: {error?.message}</div>;
+  }
+  const first = data?.results[0];
+  return first ? (
+    <Box maxW="600px" w="100%">
+      <video
+        style={{ width: "100%" }}
+        src={first.data[480]}
+        controls
+        poster={first.preview}
+      />
+    </Box>
+  ) : null;
+};
+
+export default GameTrailer;
