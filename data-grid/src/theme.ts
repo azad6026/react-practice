dba03094795e78bd1b9851c55fe0bd1b0c44fecb
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-03-04 12:29:15 +1100

    Updated theeme bakcgund

--- data-grid/src/theme.ts
+++ data-grid/src/theme.ts
@@ -20,6 +20,21 @@ const theme = extendTheme({
       900: "#111",
     },
   },
+  styles: {
+    global: (props: { colorMode: string }) => ({
+      body: {
+        background:
+          props.colorMode === "dark"
+            ? `radial-gradient(circle at 35% 50%, rgba(5,88,255,0.25), transparent 50%),
+               radial-gradient(circle at 65% 50%, rgba(128,0,74,0.25), transparent 50%),
+               radial-gradient(circle at 50% 65%, rgba(74,128,0,0.25), transparent 50%)`
+            : `radial-gradient(circle at 35% 50%, rgba(5,88,255,0.15), transparent 50%),
+               radial-gradient(circle at 65% 50%, rgba(128,0,74,0.15), transparent 50%),
+               radial-gradient(circle at 50% 65%, rgba(74,128,0,0.15), transparent 50%)`,
+        backgroundAttachment: "fixed",
+      },
+    }),
+  },
 });
 
 export default theme;
