9b973a4976371c46da4f438f8ccb8577388369c5
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-03-05 13:27:12 +1100

    Added screenshot to detail page

--- data-grid/src/pages/GameDetailPage.tsx
+++ data-grid/src/pages/GameDetailPage.tsx
@@ -1,9 +1,10 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -16,12 +17,17 @@ const GameDetailPage = () => {
     return <div>Error: {error?.message}</div>;
   }
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText maxChars={400}>{game?.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={6}>
+      <Box>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText maxChars={400}>{game?.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </Box>
+      <Box>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </Box>
+    </SimpleGrid>
   );
 };
 
